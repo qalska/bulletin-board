558e71ee2e7d3df8a6fbddd731b9434ad106ed63
qalska <qalska7@gmail.com>
2021-07-17 23:59:36 +0300

    function create a new adм

--- app/Http/Controllers/CreateANewAdController.php
+++ app/Http/Controllers/CreateANewAdController.php
@@ -17,7 +17,19 @@ class CreateANewAdController extends Controller
     }
 
     public function CreateANewAd(Request $request) {
+        $ad = new Ad();
+        $ad['title'] = $request['title'];
+        $ad['category_id'] = $request['category_id'];
+        $ad['price'] = $request['price'];
+        $ad['text'] = $request['text'];
 
+        if( $request[file('image')] ){
+            $path = Storage::putfile('public', $request[file('image')]);
+            $url = Storage::url($path);
+            $ad['image'] = $request[$url];
+        }
+
+        $ad->save();
         return view('pages.create-a-new-ad');
     }
 }
